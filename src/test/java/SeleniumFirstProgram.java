a7c2b7a9c8e1778f576cf6d406003e8c85c94bd3
warech <warech.doctorkoi@gmail.com>
2024-06-01 12:03:54 +0600

    Initial commit main branch

--- src/test/java/SeleniumFirstProgram.java
+++ src/test/java/SeleniumFirstProgram.java
@@ -1,7 +1,4 @@
-package Selenium;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumFirstProgram {
